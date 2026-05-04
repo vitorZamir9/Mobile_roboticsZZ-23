609bcbdeecfa5d8d27c85eb2f4edbe940fec7a88
Vitor Zamir <vitorzamirzamir@gmail.com>
2026-05-04 14:43:40 -0300

    Add functions to count occurrences and find size and max

--- Kotlin_exemples/04-05 atividade.kt
+++ Kotlin_exemples/04-05 atividade.kt
@@ -5,9 +5,14 @@ fun main(){
     val alvo = readln().toInt()
     ///////////////////////////////////
     val resultado = buscarLinear(lista, alvo)
+    val contagem= contarQuantasVezes(lista,alvo)
+    val tamanho = tamanhoDaLista(lista)
+    val maior= maiorValor(lista)
     println("saida: ")
     println("alvo: $alvo")
-    println("Esta na posicao: $resultado")
+    println("tamanho: $tamanho")
+    println("O alvo $alvo Esta na posicao: $resultado.\nO alvo $alvo apareceu $contagem vezes \nNa lista o maior valor" +
+            " e: $maior ")
 }
 fun buscarLinear(lista: List<Int>, alvo: Int): Int{
     //fala em qual posição está
@@ -17,4 +22,25 @@ fun buscarLinear(lista: List<Int>, alvo: Int): Int{
         }
     }
     return -1
+}
+fun contarQuantasVezes( lista: List<Int>, alvo: Int): Int{
+    var contador = 0
+    for (item in lista){
+        if (item == alvo)++contador
+    }
+    return contador
+}
+fun tamanhoDaLista(lista: List<Int>): Int{
+    var tamanho = 0
+    for (item in lista){
+        tamanho++
+    }
+    return tamanho
+}
+fun maiorValor(lista: List<Int>): Int{
+    var maior = lista[0]
+    for (item in lista){
+        if (item > maior) maior = item
+    }
+    return maior
 }
