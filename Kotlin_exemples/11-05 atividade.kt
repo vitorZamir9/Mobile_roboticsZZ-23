02729d0a416dc6d2294a8b2915af9dd53f7579c0
Vitor Zamir <vitorzamirzamir@gmail.com>
2026-05-08 14:04:27 -0300

    Refactor catalog management functions and UI
    v2

--- Kotlin_exemples/11-05 atividade.kt
+++ Kotlin_exemples/11-05 atividade.kt
@@ -9,6 +9,7 @@ fun main (){
     var id2= 0
     var autor = 0
     var funcao = 0
+    var contador= 0
 
     while (0 == 0) {
         if (funcao == 0){
@@ -19,9 +20,7 @@ fun main (){
                     "\n(3)-Buscar livro" +
                     "\n(4)-Exibir catalogo" +
                     "\n(5)-Excluir livro" +
-                    "\n(6)-Limpar catalogo" +
-                    "\n(7)-Verificar erros" +
-                    "\n(8)-Ordenar catalogo")
+                    "\n(6)-Limpar catalogo")
             println("Digite aqui: ")
             var funcao2 = readln().toInt()
             if (funcao2 == 1){//sair
@@ -30,7 +29,6 @@ fun main (){
                 break
             }
             if (funcao2 == 2){//Adicionar livro
-                var contador= 0
                 println("Digite o Nome do autor:")
                 var autor = readln().toString()
                 println("Digite o nome do livro: ")
@@ -49,21 +47,31 @@ fun main (){
                 sleep(2000)
             }
             if (funcao2 == 4){//Exibir catalogo
-
+                println("Aqui esta seu catalogo: ${exibirCatalogo(catalogo)}")
+                sleep(2000)
             }
             if (funcao2 == 5){//Excluir livro
-
+                println("Digite o id do livro que voce deseja excluir: ")
+                val exlui = readln().toInt()
+                println("livro que voce excluiu foi: ${buscarLivro(catalogo, exlui)}")
+                excluirLivro(catalogo, exlui)
+                println("Livro excluido")
             }
             if (funcao2 == 6){//Limpar catalogo
-
+                println("Voce deseja excluir o catalogo?" +
+                        "\nDigite (1) para sim." +
+                        "\nDigite (0) para cancelar.")
+                val apagaCatalogo = readln().toInt()
+                if (apagaCatalogo == 1){
+                    println("Catalogo limpo!")
+                    limparCatalogo(catalogo)
+                }else if(apagaCatalogo == 0){
+                    //
+                }else{
+                    println("Acao nao concluida")
+                }
             }
-            if (funcao2 == 7){//Verificar erros
-
-            }
-            if (funcao2 == 8){//Ordenar catalogo
-
-            }
-            if (funcao2 >= 9){
+            if (funcao2 >= 7){
                 println("Essa funcao nao existe!")
                 sleep(2000)
             }
@@ -102,13 +110,10 @@ fun exibirCatalogo(catalogo: Map<Int, Pair<String, String>>) {
         println("$id ${dados.first} ${dados.second}")
     }
 }
-fun inserirSeguro(catalogo: MutableMap<Int, String>, id: Int,
-                  nome: String) {
-    if (!catalogo.containsKey(id)) {
-        catalogo[id] = nome
-    }
+fun excluirLivro(catalogo: MutableMap<Int, Pair<String, String>>, id: Int) {
+    catalogo.remove(id)
 }
-fun ordenar(catalogo: Map<Int, String>) {
-    val ordenado = catalogo.toSortedMap()
-    println(ordenado)
+
+fun limparCatalogo(catalogo: MutableMap<Int, Pair<String, String>>) {
+    catalogo.clear()
 }
