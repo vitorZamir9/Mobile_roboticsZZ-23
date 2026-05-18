6701e7fe2363f07b17a7e5ab907b3daae9a35e1b
Vitor Zamir <vitorzamirzamir@gmail.com>
2026-05-18 13:33:34 -0300

    Update CourseLevel.kt

--- Kotlin_exemples/Semana2/CourseLevel.kt
+++ Kotlin_exemples/Semana2/CourseLevel.kt
@@ -1,1 +1,15 @@
+package model
 
+data class Course(
+    val id: Int,
+    val title: String,
+    val workloadHours: Int,
+    val level: CourseLevel
+) {
+    init {
+        // Validação de invariantes internas para evitar dados inconsistentes no sistema
+        require(id > 0) { "O id do curso deve ser positivo." }
+        require(title.isNotBlank()) { "O título do curso é obrigatório." }
+        require(workloadHours > 0) { "A carga horária deve ser maior que zero." }
+    }
+}
